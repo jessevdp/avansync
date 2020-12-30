50681617755774a9fe78dc53920d61db1af0473d
Jesse van der Pluijm <jessevdp@hey.com>
2020-12-30 11:16:31 +0100

    Use different version of std::filesystem based on what was found on system

--- server/src/command/DirectoryListingCommand.cpp
+++ server/src/command/DirectoryListingCommand.cpp
@@ -1,8 +1,12 @@
 #include "DirectoryListingCommand.hpp"
 
+#ifdef CXX_FILESYSTEM_IS_EXPERIMENTAL
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#else
 #include <filesystem>
-
 namespace fs = std::filesystem;
+#endif
 
 namespace avansync::server::command
 {
