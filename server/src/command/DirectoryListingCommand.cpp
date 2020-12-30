1f134817f25f4f9dbe3de7fba23857b07054aa8b
Jesse van der Pluijm <jessevdp@hey.com>
2020-12-30 14:33:48 +0100

    Move filesystem dependency to common-lib

--- server/src/command/DirectoryListingCommand.cpp
+++ server/src/command/DirectoryListingCommand.cpp
@@ -1,14 +1,7 @@
 #include "DirectoryListingCommand.hpp"
 
+#include "filesystem/fs.hpp"
 #include <iomanip>
-
-#ifdef CXX_FILESYSTEM_IS_EXPERIMENTAL
-#include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
-#else
-#include <filesystem>
-namespace fs = std::filesystem;
-#endif
 
 namespace avansync::server::command
 {
