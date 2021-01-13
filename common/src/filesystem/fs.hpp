73e398848059c4610776f69ae5c73de7a5f69e33
Jesse van der Pluijm <jessevdp@hey.com>
2021-01-13 15:20:00 +0100

    :green_heart: Attempt fix

--- common/src/filesystem/fs.hpp
+++ common/src/filesystem/fs.hpp
@@ -2,34 +2,38 @@
 
 #ifdef CXX_FILESYSTEM_IS_EXPERIMENTAL
 #include <experimental/filesystem>
+
 namespace fs = std::experimental::filesystem;
 
-fs::path std::experimental::filesystem::relative(const fs::path& path, const fs::path& base_path)
+namespace std::experimental::filesystem
 {
-  // 1. convert p and base to absolute paths
-  auto p = fs::absolute(path);
-  auto base = fs::absolute(base_path);
+  path relative(const path& subject_path, const path& base_path)
+  {
+    // 1. convert p and base to absolute paths
+    auto p = absolute(subject_path);
+    auto base = absolute(base_path);
 
-  // 2. find first mismatch and shared root path
-  auto mismatched = std::mismatch(p.begin(), p.end(), base.begin(), base.end());
+    // 2. find first mismatch and shared root path
+    auto mismatched = std::mismatch(p.begin(), p.end(), base.begin(), base.end());
 
-  // 3. if no mismatch return "."
-  if (mismatched.first == p.end() && mismatched.second == base.end()) return ".";
+    // 3. if no mismatch return "."
+    if (mismatched.first == p.end() && mismatched.second == base.end()) return ".";
 
-  auto it_p = mismatched.first;
-  auto it_base = mismatched.second;
+    auto it_p = mismatched.first;
+    auto it_base = mismatched.second;
 
-  fs::path ret;
+    path ret;
 
-  // 4. iterate abase to the shared root and append "../"
-  for (; it_base != base.end(); ++it_base)
-    ret /= "..";
+    // 4. iterate abase to the shared root and append "../"
+    for (; it_base != base.end(); ++it_base)
+      ret /= "..";
 
-  // 5. iterate from the shared root to the p and append its parts
-  for (; it_p != p.end(); ++it_p)
-    ret /= *it_p;
+    // 5. iterate from the shared root to the p and append its parts
+    for (; it_p != p.end(); ++it_p)
+      ret /= *it_p;
 
-  return ret;
+    return ret;
+  }
 }
 
 #else
