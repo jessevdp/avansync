dc32a89c1066944699edaa64248638ffb51bfbbb
Jesse van der Pluijm <jessevdp@hey.com>
2021-01-13 16:19:25 +0100

    :white_check_mark: Add test for InfoCommand

---
+++ server/tests/command/InfoCommandTest.cpp
@@ -0,0 +1,26 @@
+#include <catch.hpp>
+#include <fakeit.hpp>
+
+#include "command/InfoCommand.hpp"
+
+using namespace avansync;
+using namespace avansync::server;
+using namespace avansync::server::command;
+using namespace fakeit;
+
+TEST_CASE("InfoCommand")
+{
+  Mock<Connection> mock_connection;
+  Fake(Method(mock_connection, write_line));
+
+  Mock<Context> mock_context;
+  When(Method(mock_context, connection)).AlwaysReturn(mock_connection.get());
+
+  InfoCommand command;
+
+  SECTION("writes a single line of info to the client")
+  {
+    command.execute(mock_context.get());
+    Verify(Method(mock_connection, write_line)).Once();
+  }
+}
--- server/tests/example-test.cpp
+++
@@ -1,6 +0,0 @@
-#include <catch.hpp>
-
-TEST_CASE("example-test")
-{
-  REQUIRE(true == true);
-}
