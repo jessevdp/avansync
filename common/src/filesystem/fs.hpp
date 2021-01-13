02e0298d0314b78c4f8465817410303e7935a115
Jesse van der Pluijm <jessevdp@hey.com>
2021-01-13 15:27:14 +0100

    :green_heart: Attempt fix

--- common/src/filesystem/fs.hpp
+++ common/src/filesystem/fs.hpp
@@ -7,33 +7,7 @@ namespace fs = std::experimental::filesystem;
 
 namespace std::experimental::filesystem
 {
-  path relative(const path& subject_path, const path& base_path)
-  {
-    // 1. convert p and base to absolute paths
-    auto p = absolute(subject_path);
-    auto base = absolute(base_path);
-
-    // 2. find first mismatch and shared root path
-    auto mismatched = std::mismatch(p.begin(), p.end(), base.begin(), base.end());
-
-    // 3. if no mismatch return "."
-    if (mismatched.first == p.end() && mismatched.second == base.end()) return ".";
-
-    auto it_p = mismatched.first;
-    auto it_base = mismatched.second;
-
-    path ret;
-
-    // 4. iterate abase to the shared root and append "../"
-    for (; it_base != base.end(); ++it_base)
-      ret /= "..";
-
-    // 5. iterate from the shared root to the p and append its parts
-    for (; it_p != p.end(); ++it_p)
-      ret /= *it_p;
-
-    return ret;
-  }
+  path relative(const path& subject_path, const path& base_path);
 }
 
 #else
